b60b5a78132ddd350e4f65970b2965f3a174fd92
somay007 <136879645+somay007@users.noreply.github.com>
2025-04-05 09:10:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_windows_web_app" "serviceApp" {
   resource_group_name = azurerm_resource_group.rgasp.name
   site_config {
     application_stack {
-      dotnet_version = "8.0"
+      dotnet_version = "v8.0"
     }
 }
     
